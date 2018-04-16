ec9ee6a175e267b854a411139c81956b5b4900c5
ArielOrtega <arielortegabrenes98@gmail.com>
2018-04-15 18:20:12 -0600

    Se crea clase principal con formulario de JFrame y primera idea de interfaz

---
+++ Interface/Main.java
@@ -0,0 +1,14 @@
+
+package Interface;
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Ventana vn = new Ventana();
+        vn.setVisible(true);
+    }
+    
+}
